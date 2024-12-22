67ef14ac1543ac065519bfae21348d1d11e2ee2d
John Sosoka <92633120+johnsosoka@users.noreply.github.com>
2024-12-22 15:45:45 -0700

    use global acm cert for domains

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,5 +16,5 @@ variable "websites" {
 locals {
   deployer_user_name = "github-deployer-user"
   root_zone_id = data.terraform_remote_state.jscom_common_data.outputs.root_johnsosokacom_zone_id
-  acm_cert_id = data.terraform_remote_state.jscom_common_data.outputs.jscom_acm_cert
+  acm_cert_id = data.terraform_remote_state.jscom_common_data.outputs.jscom_acm_cert_global
 }
