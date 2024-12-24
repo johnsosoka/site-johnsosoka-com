08c1fbfdca8a4f9ea54a95ac6001e3ac67a4e6ad
John Sosoka <92633120+johnsosoka@users.noreply.github.com>
2024-12-24 11:14:16 -0700

    provision root api from blog/web repo.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,4 +17,5 @@ locals {
   deployer_user_name = "github-deployer-user"
   root_zone_id = data.terraform_remote_state.jscom_common_data.outputs.root_johnsosokacom_zone_id
   acm_cert_id = data.terraform_remote_state.jscom_common_data.outputs.jscom_acm_cert_global
+  acm_cert_regional = data.terraform_remote_state.jscom_common_data.outputs.jscom_acm_cert
 }
